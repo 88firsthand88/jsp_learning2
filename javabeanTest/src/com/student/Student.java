66e606b6d0eebdc2d4b5047cbc986d8997833c85
88firsthand88 <845398176@qq.com>
2015-08-31 20:28:23 +0800

    study

---
+++ javabeanTest/src/com/student/Student.java
@@ -0,0 +1,19 @@
+package com.student;
+
+public class Student {
+	private String name;
+	private String password;
+	public Student(){}
+	public void setName(String name){
+		this.name = name;
+	}
+	public String getName(){
+		return this.name;
+	}
+	public void setPassword(String password){
+		this.password = password;
+	}
+	public String getPassword(){
+		return this.password;
+	}
+}
